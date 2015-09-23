464d51fdc9ec58f4c5535241dbd523638a682937
wrongerror <419399465@qq.com>
2015-09-23 10:58:52 +0800

    middleware

--- profiling/middleware.py
+++ profiling/middleware.py
@@ -5,14 +5,16 @@ import re
 Redirect_USER_AGENTS = (re.compile(r'micromessenger', re.IGNORECASE), )
 class RedirectMiddleware(object):
     def process_request(self, request):
-        if 'HTTP_USER_AGENT' in request.META:
-            for user_agent_regex in Redirect_USER_AGENTS:
-                if not user_agent_regex.search(request.META['HTTP_USER_AGENT']):
-                    return HttpResponse(
-                        '<body style="margin-left:25%;margin-right:25%;marigin-top:80px;text-align: center; ">\
-                        <div >\
-                        <h2>请在微信中打开 :)</h2>\
-                        </div>\
-                        </body>\
-                        ', 
-                        content_type='text/html; charset=utf-8')
+        path = request.path.split('/')[1]
+        if not  (path == 'admin'):
+            if 'HTTP_USER_AGENT' in request.META:
+                for user_agent_regex in Redirect_USER_AGENTS:
+                    if user_agent_regex.search(request.META['HTTP_USER_AGENT']):
+                        return HttpResponse(
+                            '<body style="margin-left:25%;margin-right:25%;marigin-top:80px;text-align: center; ">\
+                            <div >\
+                            <h2>请在微信中打开 :)</h2>\
+                            </div>\
+                            </body>\
+                            ', 
+                            content_type='text/html; charset=utf-8')
